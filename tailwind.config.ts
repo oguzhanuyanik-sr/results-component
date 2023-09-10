97c2242de97440b139980eef16f6463f5f12814f
oguzhanuyanik-sr <oguzhanuyanik.sr@gmail.com>
2023-09-10 09:35:05 +0300

    project created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,14 +7,39 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    colors: {
+      yellow: '#FFB21E',
+      white: '#FFFFFF',
+      red: '#FF5555',
+      green: '#00BB8F',
+      blue: '#1125D6',
+      darkBlue: '#303B59',
+      lightBlue: '#CAC9FF',
+      veryLightBlue: '#ECF2FF',
+    },
+    fontFamily: {
+      space: 'var(--font-space)',
+      lexend: 'var(--font-lexend)',
+    },
+    boxShadow: {},
+    container: {
+      center: true,
+      padding: '4rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
       },
     },
+    extend: {},
   },
   plugins: [],
-}
-export default config
+};
+export default config;
